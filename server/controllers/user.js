cd1c881aa4febfb596a002980fdbaae815c74b0f
KiteRunner2 <36109470+KiteRunner2@users.noreply.github.com>
2020-05-31 23:52:28 -0400

    code formatting

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,9 +1,14 @@
-const db = require("../models");
-let user = require("../user.json");
-const bcrypt = require("bcrypt");
-const utils = require("../utils");
+const db = require('../models');
+let user = require('../user.json');
+const bcrypt = require('bcrypt');
+const utils = require('../utils');
 
 exports.updateUserProfile = async (req, res, next) => {
+  if (!req.session.isLoggedIn) {
+    res.json({
+      error: 'user not logged in',
+    });
+  }
   const response = await db.userprofile.findOneAndReplace(
     { email: req.body.email },
     req.body
@@ -11,20 +16,26 @@ exports.updateUserProfile = async (req, res, next) => {
   res.json(response);
 };
 
+exports.userLogout = async (req, res, next) => {
+  req.session.destroy((err) => {
+    console.log(err);
+    res.redirect('/');
+  });
+};
+
 exports.getUser = async (req, res, next) => {
   const query = { email: req.params.email };
   let response = await db.userprofile.find(query);
   response = JSON.stringify(response);
   response = JSON.parse(response);
-  console.log(response);
   const sharedDashboardsTo = await db.userprofile.find(
     {
-      "sharedByUser.to": req.params.email,
+      'sharedByUser.to': req.params.email,
     },
     {
       _id: 0,
       email: 1,
-      "sharedByUser.$": 1,
+      'sharedByUser.$': 1,
       dashboards: 1,
       firstname: 1,
       lastname: 1,
@@ -32,13 +43,22 @@ exports.getUser = async (req, res, next) => {
   );
 
   let reply = [];
-  reply.push(utils.removePassword(response));
+  reply.push(response);
   reply.push(sharedDashboardsTo);
 
   if (response.length > 0) {
-    console.log(reply);
     res.json(reply);
-  } else res.json({ answer: "nothing found" });
+  } else res.json({ answer: 'nothing found' });
+};
+
+exports.postLogin = (req, res, next) => {
+  req.session.isLoggedIn = true;
+  const response = {
+    message: 'OK',
+    email: req.email,
+  };
+  //   res.redirect('/projectdashboard');
+  res.json(response);
 };
 
 exports.login = async (req, res, next) => {
@@ -51,8 +71,7 @@ exports.login = async (req, res, next) => {
     email: 1,
     password: 1,
   });
-
-  console.log(typeof userProfile);
+  console.log('loggind userProfile', userProfile);
   let response;
   if (userProfile) {
     userProfile = JSON.stringify(userProfile);
@@ -62,19 +81,24 @@ exports.login = async (req, res, next) => {
         req.body.password,
         userProfile[0].password
       );
+      console.log('isValidPassword', isValidPassword);
       if (isValidPassword) {
         response = {
-          message: "OK",
+          message: 'OK',
           email: userProfile[0].email,
         };
+        req.email = userProfile[0].email;
+        next();
       } else {
-        response = { message: "Invalid username/password" };
+        response = { message: 'Invalid username/password' };
+        res.json(response);
       }
     } else {
-      response = { message: "Invalid username/password" };
+      response = { message: 'Invalid username/password' };
+      res.json(response);
     }
   } else {
-    response = { message: "Database error" };
+    response = { message: 'Database error' };
+    res.json(response);
   }
-  res.json(response);
 };
